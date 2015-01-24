92ef617f868255ff631695806435700e3fabc798
tomas <plasil.tomas@seznam.cz>
2015-01-24 18:28:13 +0100

    oprava testu

--- test/SystemTest/Model/UserTest.php
+++ test/SystemTest/Model/UserTest.php
@@ -14,15 +14,15 @@ class UserTest extends PHPUnit_Framework_TestCase {
         'surname' => 'surname',
         'email' => 'email',
         'last_login' => 'last_login',
-        'id_role' => 123,
-        'is_admin' => true);
+        'is_admin' => true,
+        'is_active' => true);
     $user->exchangeArray($data);
     $this->assertSame($data['name'], $user->name, '"name" was not set correctly');
     $this->assertSame($data['surname'], $user->surname, '"surname" was not set correctly');
     $this->assertSame($data['email'], $user->email, '"email" was not set correctly');
     $this->assertSame($data['last_login'], $user->last_login, '"last_login" was not set correctly');
-    $this->assertSame($data['id_role'], $user->id_role, '"id_role" was not set correctly');
     $this->assertSame($data['is_admin'], $user->is_admin, '"is_admin" was not set correctly');
+    $this->assertSame($data['is_active'], $user->is_active, '"is_active" was not set correctly');
   }
 
   public function testExchangeArraySetsPropertiesToNullIfKeysAreNotPresent() {
@@ -33,15 +33,13 @@ class UserTest extends PHPUnit_Framework_TestCase {
         'surname' => 'surname',
         'email' => 'email',
         'last_login' => 'last_login',
-        'id_role' => 123,
         'is_admin' => true);
     $user->exchangeArray($data);
     $user->exchangeArray(array('id_user' => 1));
     $this->assertNull($user->name, '"name" should have defaulted to null');
     $this->assertNull($user->surname, '"surname" should have defaulted to null');
     $this->assertNull($user->email, '"email" should have defaulted to null');
-    $this->assertNull($user->last_login, '"last_login" should have defaulted to null');
-    $this->assertObjectNotHasAttribute('id_role', $user, '"id_role" should hnot exist');
+    $this->assertObjectNotHasAttribute('last_login', $user, '"llast_login" should not exist');
     $this->assertObjectNotHasAttribute('is_admin', $user, '"is_admin" should not exist');
   }
 
