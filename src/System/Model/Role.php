dcf283ba019dd35009d7566db6a7aa90c14edbb5
tomas <tomas@plasil-ThinkPad-T430>
2015-11-21 10:50:34 +0100

    doplnění interfejsu pro zápis a čtení dat modelu

--- src/System/Model/Role.php
+++ src/System/Model/Role.php
@@ -7,66 +7,61 @@ use Zend\InputFilter\InputFilter;
 use Zend\InputFilter\InputFilterAwareInterface;
 use Zend\InputFilter\InputFilterInterface;
 
-class Role implements InputFilterAwareInterface
-{
-    public $id_role;
-    public $name;
-    protected $inputFilter;
+class Role implements InputFilterAwareInterface, \Zend\Stdlib\ArraySerializableInterface {
 
-    public function exchangeArray($data)
-    {
-        $this->id_role = (isset($data['id_role'])) ? $data['id_role'] : null;
-        $this->name = (isset($data['name'])) ? $data['name'] : null;
-    }
-    
-    public function getArrayCopy()
-    {
-        return get_object_vars($this);
-    }
+  public $id_role;
+  public $name;
+  protected $inputFilter;
 
-    
-    public function setInputFilter(InputFilterInterface $inputFilter)
-    {
-        throw new \Exception("Not used");
-    }
-    
-    public function getInputFilter()
-    {
-        if (!$this->inputFilter) {
-            $inputFilter = new InputFilter();
-            $factory     = new InputFactory();
+  public function exchangeArray(array $data) {
+    $this->id_role = (isset($data['id_role'])) ? $data['id_role'] : null;
+    $this->name = (isset($data['name'])) ? $data['name'] : null;
+  }
 
-            $inputFilter->add($factory->createInput(array(
-                'name'     => 'id_role',
+  public function getArrayCopy() {
+    return get_object_vars($this);
+  }
+
+  public function setInputFilter(InputFilterInterface $inputFilter) {
+    throw new \Exception("Not used");
+  }
+
+  public function getInputFilter() {
+    if (!$this->inputFilter) {
+      $inputFilter = new InputFilter();
+      $factory = new InputFactory();
+
+      $inputFilter->add($factory->createInput(array(
+                'name' => 'id_role',
                 'required' => true,
-                'filters'  => array(
-                    array('name' => 'Int'),
+                'filters' => array(
+                  array('name' => 'Int'),
                 ),
-            )));
+      )));
 
-            $inputFilter->add($factory->createInput(array(
-                'name'     => 'name',
+      $inputFilter->add($factory->createInput(array(
+                'name' => 'name',
                 'required' => true,
-                'filters'  => array(
-                    array('name' => 'StripTags'),
-                    array('name' => 'StringTrim'),
+                'filters' => array(
+                  array('name' => 'StripTags'),
+                  array('name' => 'StringTrim'),
                 ),
                 'validators' => array(
-                    array(
-                        'name'    => 'StringLength',
-                        'options' => array(
-                            'encoding' => 'UTF-8',
-                            'min'      => 1,
-                            'max'      => 25,
-                        ),
+                  array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                      'encoding' => 'UTF-8',
+                      'min' => 1,
+                      'max' => 25,
                     ),
+                  ),
                 ),
-            )));
+      )));
 
-            $this->inputFilter = $inputFilter;
-        }
-
-        return $this->inputFilter;
+      $this->inputFilter = $inputFilter;
     }
-    
+
+    return $this->inputFilter;
+  }
+
 }
