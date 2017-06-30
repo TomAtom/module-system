fccbc9d8dee1479dd0a3a8bf1c991bc6da717668
tomas <tomas@tombook>
2017-06-30 23:05:59 +0200

    format

--- config/module.config.php
+++ config/module.config.php
@@ -1,80 +1,79 @@
 <?php
-
 return array(
-    'controllers' => array(
-        'factories' => [
-            'System\Controller\Role' => 'System\Controller\Factory\RoleController',
-            'System\Controller\Authentification' => 'System\Controller\Factory\AuthentificationController',
-            'System\Controller\User' => 'System\Controller\Factory\UserController',
-        ]
-    ),
-    'router' => array(
-        'routes' => array(
-            'user' => array(
-                'type'    => 'segment',
-                'options' => array(
-                    'route'    => '/user[/action/:action][/id/:id][/page/:page]',
-                    'constraints' => array(
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                        'page'     => '[0-9]+',
-                    ),
-                    'defaults' => array(
-                        'controller' => 'System\Controller\User',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
-            'authentification' => array(
-                'type'    => 'segment',
-                'options' => array(
-                    'route'    => '/authentification[/:action]',
-                    'constraints' => array(
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                    ),
-                    'defaults' => array(
-                        'controller' => 'System\Controller\Authentification',
-                        'action'     => 'login',
-                    ),
-                ),
-            ),
-            'role' => array(
-                'type'    => 'segment',
-                'options' => array(
-                    'route'    => '/role[/:action][/:id]',
-                    'constraints' => array(
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ),
-                    'defaults' => array(
-                        'controller' => 'System\Controller\Role',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
+  'controllers' => array(
+    'factories' => [
+      'System\Controller\Role' => 'System\Controller\Factory\RoleController',
+      'System\Controller\Authentification' => 'System\Controller\Factory\AuthentificationController',
+      'System\Controller\User' => 'System\Controller\Factory\UserController',
+    ]
+  ),
+  'router' => array(
+    'routes' => array(
+      'user' => array(
+        'type' => 'segment',
+        'options' => array(
+          'route' => '/user[/action/:action][/id/:id][/page/:page]',
+          'constraints' => array(
+            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+            'id' => '[0-9]+',
+            'page' => '[0-9]+',
+          ),
+          'defaults' => array(
+            'controller' => 'System\Controller\User',
+            'action' => 'index',
+          ),
         ),
-    ),
-    'view_manager' => array(
-        'template_path_stack' => array(
-            'system' => __DIR__ . '/../view',
+      ),
+      'authentification' => array(
+        'type' => 'segment',
+        'options' => array(
+          'route' => '/authentification[/:action]',
+          'constraints' => array(
+            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+          ),
+          'defaults' => array(
+            'controller' => 'System\Controller\Authentification',
+            'action' => 'login',
+          ),
         ),
-        'template_map' => array(
-            'layout/system/unauthorizedAccess'  => __DIR__ . '/../view/layout/unauthorizedAccess.phtml',
+      ),
+      'role' => array(
+        'type' => 'segment',
+        'options' => array(
+          'route' => '/role[/:action][/:id]',
+          'constraints' => array(
+            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+            'id' => '[0-9]+',
+          ),
+          'defaults' => array(
+            'controller' => 'System\Controller\Role',
+            'action' => 'index',
+          ),
         ),
+      ),
     ),
-    'service_manager' => array(
-        'abstract_factories' => array(
-            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
-        ),
+  ),
+  'view_manager' => array(
+    'template_path_stack' => array(
+      'system' => __DIR__ . '/../view',
     ),
-    'caches' => array(
-        'CacheAcl' => array(
-            'adapter' => array(
-                'name' => 'filesystem'
-            ),
-            'options' => array(
-                'cache_dir' => "./data/cache/aclcache",
-            ),
-        ),
+    'template_map' => array(
+      'layout/system/unauthorizedAccess' => __DIR__ . '/../view/layout/unauthorizedAccess.phtml',
     ),
+  ),
+  'service_manager' => array(
+    'abstract_factories' => array(
+      'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+    ),
+  ),
+  'caches' => array(
+    'CacheAcl' => array(
+      'adapter' => array(
+        'name' => 'filesystem'
+      ),
+      'options' => array(
+        'cache_dir' => "./data/cache/aclcache",
+      ),
+    ),
+  ),
 );
